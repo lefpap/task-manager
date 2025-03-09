9aeeb6a61aff7d6ce9e21e65341f52a52e18e68f
lefpap <lolopap21@gmail.com>
2025-03-09 17:11:31 +0200

    Initial commit

---
+++ src/test/java/com/lefpap/TaskManagerApplicationTests.java
@@ -0,0 +1,15 @@
+package com.lefpap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class TaskManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/test/java/com/lefpap/TestTaskManagerApplication.java
@@ -0,0 +1,11 @@
+package com.lefpap;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestTaskManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(TaskManagerApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
